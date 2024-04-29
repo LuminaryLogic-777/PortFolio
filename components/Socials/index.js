9d5e9082f50a1d2409b9962a35932b838918534d
sonaiClayMind <sonaiClayMind@gmail.com>
2024-04-29 18:27:06 +0530

    added blog location on socials component

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Button from "../Button";
-
+import { useRouter } from "next/router";
 import yourData from "../../data/portfolio.json";
 
 const Socials = ({ className }) => {
+  const router = useRouter();
+
+  const handleSocialClick = (link) => {
+    if (link.startsWith("http")) {
+      window.open(link, "_blank");
+    } else {
+      router.push(link);
+    }
+  };
+
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
-        <Button key={index} onClick={() => window.open(social.link)}>
+        <Button
+          key={index}
+          onClick={() => handleSocialClick(social.link)}
+        >
           {social.title}
         </Button>
       ))}
